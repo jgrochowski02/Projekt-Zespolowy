cc400a2387084d9e9839e1475443e0ad81ef7f3b
Weronika Mazurczak <weronikamazurczak202@gmail.com>
2024-04-18 16:41:14 +0200

    add: array object languages.js and display it on map()

--- src/views/Flashcards/index.js
+++ src/views/Flashcards/index.js
@@ -1,14 +1,56 @@
-import {StyleSheet, Button, Text, View, image } from 'react-native';
-
-
+import { Text, View, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import Swipeable from "react-native-gesture-handler/Swipeable";
+import { styles } from "../styles";
+import { Search, DeckAdd, Edit, Remove } from "../icons";
+import { languagesArr } from "../Flashcards/languages";
+import { useEffect } from "react";
 
 export function Flashcards({ navigation }) {
-    return (
-      
-      <View >  
-               <Text>Ekran fiszek </Text>
-        
-        </View>
-    );
-};
-export default Flashcards
+  console.log(languagesArr);
+  const [hasToRerender, setHasToRerender] = useState(false);
+
+  useEffect(() => {
+    console.log("haslogfok");
+  }, [hasToRerender]);
+
+  return (
+    <>
+      {languagesArr.map((language, index) => (
+        <Swipeable
+          key={index}
+          style={styles.list}
+          renderLeftActions={() => (
+            <TouchableOpacity style={styles.leftAction} onPress={() => {}}>
+              <Edit />
+            </TouchableOpacity>
+          )}
+          renderRightActions={() => (
+            <TouchableOpacity
+              style={styles.rightAction}
+              onPress={() => {
+                languagesArr.splice(index, 1);
+                setHasToRerender(!hasToRerender);
+              }}
+            >
+              <Remove />
+            </TouchableOpacity>
+          )}
+        >
+          <TouchableOpacity
+            style={styles.flatCard}
+            onPress={() =>
+              navigation.navigate("Fiszka", {
+                deck: language.deck,
+              })
+            }
+          >
+            <Text style={styles.deckTitle}>{language.title}</Text>
+            <Text style={styles.deckBody}>{language.cards}</Text>
+          </TouchableOpacity>
+        </Swipeable>
+      ))}
+    </>
+  );
+}
+export default Flashcards;
---
+++ src/views/Flashcards/languages.js
@@ -0,0 +1,32 @@
+export const languagesArr = [
+  {
+    title: "Flashcards",
+    deck: "English",
+    cards: " cards",
+  },
+  {
+    title: "Fiszki",
+    deck: "Polskie",
+    cards: " fiszki",
+  },
+  {
+    title: "Karteikarten",
+    deck: "Deutsch",
+    cards: " Karten",
+  },
+  {
+    title: "Flashcards",
+    deck: "Français",
+    cards: " cartes",
+  },
+  {
+    title: "Tarjetas",
+    deck: "Español",
+    cards: " tarjetas",
+  },
+  {
+    title: "Flashcards",
+    deck: "Italiano",
+    cards: " carte",
+  },
+];
