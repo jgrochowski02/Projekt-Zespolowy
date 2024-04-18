52014a57f428cec0882c6978a35a30e97145c92c
Weronika Mazurczak <weronikamazurczak202@gmail.com>
2024-04-18 16:42:39 +0200

    add: flashcard functionality and questions.js file

---
+++ src/views/Flashcards/flashcard.js
@@ -0,0 +1,114 @@
+import { View, Text, Button } from "react-native";
+import React, { useState } from "react";
+import { styles } from "../styles";
+import FlipCard from "react-native-flip-card";
+import { questions } from "../Flashcards/questions";
+import { useRoute } from "@react-navigation/native";
+import Swiper from "react-native-deck-swiper";
+
+export default function flashcard() {
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  let questionList = Object.values(questions[0].pl.questions);
+  const { deck } = useRoute().params;
+  switch (deck) {
+    case "Polskie":
+      questionList = Object.values(questions[0].pl.questions);
+      break;
+    case "English":
+      questionList = Object.values(questions[0].en.questions);
+      break;
+    case "Deutsch":
+      questionList = Object.values(questions[0].de.questions);
+      break;
+    case "Français":
+      questionList = Object.values(questions[0].fr.questions);
+      break;
+    case "Español":
+      questionList = Object.values(questions[0].es.questions);
+      break;
+    case "Italiano":
+      questionList = Object.values(questions[0].it.questions);
+      break;
+  }
+
+  const cards = questionList.map((question, index) => ({
+    question: question.ask,
+    answer: question.answer,
+    id: index,
+  }));
+
+  const renderCard = (card) => (
+    <View style={styles.view}>
+      <FlipCard
+        style={styles.flipCard}
+        perspective={1000}
+        flipHorizontal={true}
+        flipVertical={false}
+      >
+        <View style={styles.flipSide}>
+          <Text style={styles.face}>
+            {questionList[currentQuestionIndex].ask}
+          </Text>
+        </View>
+        <View style={styles.flipSide}>
+          <Text style={styles.back}>
+            {questionList[currentQuestionIndex].answer}
+          </Text>
+        </View>
+      </FlipCard>
+    </View>
+  );
+
+  return (
+    <>
+      <Swiper
+        style={{}}
+        ref={(swiper) => {
+          this.swiper = swiper;
+        }}
+        cards={cards}
+        renderCard={renderCard}
+        backgroundColor="transparent"
+        onSwiped={(cardIndex) => { setCurrentQuestionIndex(cardIndex + 1); }}
+        overlayLabels={{
+          left: {
+            title: 'NOT YET',
+            style: {
+              label: {
+                backgroundColor: "red",
+                borderColor: 'black',
+                color: 'white',
+                borderWidth: 1
+              },
+              wrapper: {
+                flexDirection: 'column',
+                alignItems: 'flex-end',
+                justifyContent: 'flex-start',
+                marginTop: 30,
+                marginLeft: -30
+              }
+            }
+          },
+          right: {
+            title: 'GOT IT',
+            style: {
+              label: {
+                backgroundColor: "green",
+                borderColor: 'black',
+                color: 'white',
+                borderWidth: 1
+              },
+              wrapper: {
+                flexDirection: 'column',
+                alignItems: 'flex-start',
+                justifyContent: 'flex-start',
+                marginTop: 30,
+                marginLeft: 30
+              }
+            }
+          },
+        }}
+      ></Swiper>
+    </>
+  );
+}
---
+++ src/views/Flashcards/questions.js
@@ -0,0 +1,22 @@
+export const questions = [
+    {
+        pl:{
+            questions: {question1:{ask: "Kot", answer: "Cat"}, question2:{ask: "Pies", answer: "Dog"}, question3:{ask: "Koń", answer: "Horse"}, question4:{ask: "Krowa", answer: "Cow"}, question5:{ask: "Kura", answer: "Chicken"}}
+        },
+        en:{
+            questions: {question1:{ask: "Cat", answer: "Kot"}, question2:{ask: "Dog", answer: "Pies"}, question3:{ask: "Horse", answer: "Koń"}, question4:{ask: "Cow", answer: "Krowa"}, question5:{ask: "Chicken", answer: "Kura"}}
+        },
+        de:{
+            questions: {question1:{ask: "Katze", answer: "Kot"}, question2:{ask: "Hund", answer: "Pies"}, question3:{ask: "Pferd", answer: "Koń"}, question4:{ask: "Kuh", answer: "Krowa"}, question5:{ask: "Huhn", answer: "Kura"}}
+        },
+        fr:{
+            questions: {question1:{ask: "Chat", answer: "Kot"}, question2:{ask: "Chien", answer: "Pies"}, question3:{ask: "Cheval", answer: "Koń"}, question4:{ask: "Vache", answer: "Krowa"}, question5:{ask: "Poulet", answer: "Kura"}}
+        },
+        es:{
+            questions: {question1:{ask: "Gato", answer: "Kot"}, question2:{ask: "Perro", answer: "Pies"}, question3:{ask: "Caballo", answer: "Koń"}, question4:{ask: "Vaca", answer: "Krowa"}, question5:{ask: "Pollo", answer: "Kura"}}
+        },
+        it:{
+            questions: {question1:{ask: "Gatto", answer: "Kot"}, question2:{ask: "Cane", answer: "Pies"}, question3:{ask: "Cavallo", answer: "Koń"}, question4:{ask: "Mucca", answer: "Krowa"}, question5:{ask: "Pollo", answer: "Kura"}}
+        }
+    }
+];
